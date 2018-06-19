a2c8d6da7fb77b6116a00bd134b80d075f6b741e
gagankdev6 <gagank@dev6.com>
2018-06-19 09:53:10 -0400

    added styles,markup and image path in ts file

--- exercise/src/app/components/home/home.component.ts
+++ exercise/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbTabContent } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  verticalLogoPath = '../../../assets/images/Shirtastic-vertical.svg';
+  fractalPath = '../../../assets/images/Fractal.png';
   constructor() { }
 
   ngOnInit() {
