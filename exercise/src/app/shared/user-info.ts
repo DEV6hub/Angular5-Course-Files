d3aafa46646fe373467940903c13e6596b98a2b3
gagankdev6 <gagank@dev6.com>
2018-06-21 09:21:02 -0400

    Added custom directives

--- exercise/src/app/shared/user-info.ts
+++ exercise/src/app/shared/user-info.ts
@@ -1,13 +1,22 @@
 export class UserInfo {
-    constructor(
-        public name: string = '',
-        public email: string = '',
-        public phone?: number,
-        public address1: string = '',
-        public address2: string = '',
-        public city: string = '',
-        public country: string = 'Select',
-        public province: string = 'Select',
-        public postal: string = '',
-    ) {}
+        public name: string;
+        public email: string;
+        public phone: number;
+        public address1: string;
+        public address2: string;
+        public city: string;
+        public country: string;
+        public province: string;
+        public postal: string;
+        constructor(userInfo: any) {
+            this.name = userInfo.name;
+            this.email = userInfo.email;
+            this.phone = userInfo.phone;
+            this.address1 = userInfo.address1;
+            this.address2 = userInfo.address2;
+            this.city = userInfo.city;
+            this.country = userInfo.country;
+            this.province = userInfo.province;
+            this.postal = userInfo.postal;
+        }
 }
