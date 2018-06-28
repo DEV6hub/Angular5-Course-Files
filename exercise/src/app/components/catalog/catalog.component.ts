c4db32426d338893a2b64cb3c2736895e275ee26
gagankdev6 <gagank@dev6.com>
2018-06-28 15:03:44 -0400

    removed unused import

--- exercise/src/app/components/catalog/catalog.component.ts
+++ exercise/src/app/components/catalog/catalog.component.ts
@@ -2,12 +2,11 @@ import { Component, OnInit, OnDestroy, Renderer2, ViewChild, ElementRef } from '
 import { ShirtGenderPipe } from '../../filters/shirt-filter';
 import { Shirt } from '../../shared/shirt';
 import { ShirtService } from '../../core/shirt.service';
-import { Subscription } from 'rxjs';
+import { Subscription } from 'rxjs/Subscription';
 import { ShoppingCartService } from '../../core/shopping-cart.service';
 import { SlidingPanelsService } from '../../core/sliding-panels.service';
 import { NgbTabset } from '@ng-bootstrap/ng-bootstrap';
 import 'fabric';
-import { networkInterfaces } from 'os';
 import { Router } from '@angular/router';
 
 declare const fabric: any;
